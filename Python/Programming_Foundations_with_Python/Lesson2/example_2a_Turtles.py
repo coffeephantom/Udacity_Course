b8322eaf08512ba1a81935e98b18b74a31c5167a
coffeephantom <coffeephantom@users.noreply.github.com>
2015-03-22 15:08:01 +0800

    将画圈的做成一个函数draw_circle

--- Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
+++ Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
@@ -1,9 +1,6 @@
 import turtle
 
 def draw_square():
-    window =turtle.Screen()
-    window.bgcolor("red")
-
     brad = turtle.Turtle("turtle")
     brad.color("white")
     brad.fillcolor("yellow")
@@ -16,12 +13,18 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+    
 
+def draw_circle():
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
-    
-    window.exitonclick()
 
+  
+    
+window =turtle.Screen()
+window.bgcolor("red")
 draw_square()
+draw_circle()
+window.exitonclick()  
