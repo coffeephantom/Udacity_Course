8cdc5e2e7081be8623274374c76bb7767531aca5
coffeephantom <coffeephantom@users.noreply.github.com>
2015-03-22 13:42:46 +0800

    完成正方形的绘制。

--- Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
+++ Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
@@ -6,7 +6,13 @@ def draw_square():
 
     brad = turtle.Turtle()
     brad.forward(100)
-
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
     window.exitonclick()
 
 draw_square()
