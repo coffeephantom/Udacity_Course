a4498966f5d71134028edb64e10d74cee347bf14
coffeephantom <coffeephantom@users.noreply.github.com>
2015-03-22 14:59:58 +0800

    添加新的turtle实例angie用以画蓝色的圈。

--- Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
+++ Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
@@ -16,6 +16,12 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+    
     window.exitonclick()
 
 draw_square()
