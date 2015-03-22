2c8871cca6c0355b69c10d9da93636692d66f92f
coffeephantom <coffeephantom@users.noreply.github.com>
2015-03-22 16:25:59 +0800

    增添turtle参数，优化绘制正方形、圆形、三角形的函数。

--- Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
+++ Python/Programming_Foundations_with_Python/Lesson2/example_2a_Turtles.py
@@ -1,34 +1,37 @@
 import turtle
 
-def draw_square():
-    brad = turtle.Turtle("turtle")
-    brad.color("white")
-    brad.fillcolor("yellow")
-    brad.speed("fast")
+def draw_square(turtle):
     for x in xrange(1,5):
-        brad.forward(100)
-        brad.right(90)
+        turtle.forward(100)
+        turtle.right(90)
     
-def draw_circle():
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
+def draw_circle(turtle):
+    turtle.circle(100)
 
-def draw_triangle():
-    fred=turtle.Turtle()
-    fred.shape("circle")
-    fred.color("black")
-    fred.forward(100)
-    fred.goto(50,50)  
-    fred.goto(0,0)
+def draw_triangle(turtle):
+    turtle.forward(100)
+    turtle.goto(50,50)  
+    turtle.goto(0,0)
 
 def draw_art():    
     window =turtle.Screen()
     window.bgcolor("red")
-    draw_square()
-    draw_circle()
-    draw_triangle()
+    brad = turtle.Turtle("turtle")
+    brad.color("white")
+    brad.fillcolor("yellow")
+    brad.speed("fast")
+    draw_square(brad)
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    draw_circle(angie)
+
+    fred=turtle.Turtle()
+    fred.shape("circle")
+    fred.color("black")
+    draw_triangle(fred)
+
     window.exitonclick()
 
 draw_art()  
