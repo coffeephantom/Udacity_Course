b3c670702624c3894a95eba5ec91ad45b16d9176
coffeephantom <coffeephantom@users.noreply.github.com>
2015-03-23 23:46:02 +0800

    跟随视频补充代码完整，但是似乎因为GFW的关系，连接有问题。

--- Python/Programming_Foundations_with_Python/Lesson2/example_2c_Profanity_Editor.py
+++ Python/Programming_Foundations_with_Python/Lesson2/example_2c_Profanity_Editor.py
@@ -1,6 +1,17 @@
+import urllib
+
 def read_text():
     quotes=open("/Users/coffeephantom/Downloads/movie_quotes.txt")
     contents_of_file=quotes.read()
+    check_profanity(contents_of_file)
     print contents_of_file
     quotes.close()
+
+
+def check_profanity(text_to_check):
+    connection=urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+
 read_text()
