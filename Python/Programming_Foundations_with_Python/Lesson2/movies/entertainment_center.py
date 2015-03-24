af27b23d29fec5c2e45a856fbece6881a1264ec8
coffeephantom <coffeephantom@users.noreply.github.com>
2015-03-25 00:27:04 +0800

    添加新实例Big Hero 6

--- Python/Programming_Foundations_with_Python/Lesson2/movies/entertainment_center.py
+++ Python/Programming_Foundations_with_Python/Lesson2/movies/entertainment_center.py
@@ -5,5 +5,11 @@ toy_story = media.Movie("Toy Story","A story about a boy and his toy that come i
 
 avatar = media.Movie("Avatar","A marine on an alian planet","http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-print toy_story.storyline
-avatar.show_trailer()
+big_hero_6 = media.Movie ("Big Hero 6","The adventure of junior robotic genius, health care robot, and other friends","https://en.wikipedia.org/wiki/Big_Hero_6_(film)#/media/File:Big_Hero_6_(film)_poster.jpg","https://www.youtube.com/watch?v=z3biFxZIJOQ")
+
+#print toy_story.storyline
+#avatar.show_trailer()
+
+print big_hero_6.title
+print big_hero_6.storyline
+big_hero_6.show_trailer()
