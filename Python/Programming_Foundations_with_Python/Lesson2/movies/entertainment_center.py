357b8d4e0fcedc29c288b4fc355629e89455aa32
coffeephantom <coffeephantom@users.noreply.github.com>
2015-03-24 23:59:35 +0800

    添加新实例avatar

--- Python/Programming_Foundations_with_Python/Lesson2/movies/entertainment_center.py
+++ Python/Programming_Foundations_with_Python/Lesson2/movies/entertainment_center.py
@@ -1,5 +1,6 @@
 import media
 
 toy_story = media.Movie("Toy Story","A story about a boy and his toy that come into life",
-    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","http://www.youtube.com/watch?v=vwyZH85NQC4")
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=vwyZH85NQC4")
+avatar = media.Movie("Avatar","A marine on an alian planet","http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")
 print toy_story.storyline
